209a5a8c4891ed48ccacc9f240ebeaba655a6f81
hyunwoo.park <sweat0713@gmail.com>
2025-09-15 14:53:06 +0900

    02-소수(에라토스테네스 체)

---
+++ src/main/java/org/example/section02/Main05.java
@@ -0,0 +1,50 @@
+package org.example.section02;
+
+import java.util.Scanner;
+
+public class Main05 {
+
+    public int solution(int n) {
+        int answer = 0;
+        int[] ch = new int[n + 1];
+        for(int i=2; i<=n; i++) {
+            if(ch[i] == 0) {
+                answer ++;
+                for(int j = i; j <= n; j=j+i) {
+                    ch[j] = 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main05 T = new Main05();
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
+/**
+ * 5. 소수(에라토스테네스 체)
+ *
+ * 설명
+ * 자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
+ *
+ * 만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
+ *
+ * 입력
+ * 첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
+ *
+ * 출력
+ * 첫 줄에 소수의 개수를 출력합니다.
+ *
+ * 예시 입력 1
+ * 20
+ *
+ * 예시 출력 1
+ * 8
+ */
