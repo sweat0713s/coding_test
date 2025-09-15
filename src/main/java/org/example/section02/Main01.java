133d5fdf0d5203b2a6ef9d06ca318c53f5e24244
hyunwoo.park <sweat0713@gmail.com>
2025-09-15 11:39:26 +0900

    02-큰 수 출력하기

---
+++ src/main/java/org/example/section02/Main01.java
@@ -0,0 +1,54 @@
+package org.example.section02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main01 {
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if(arr[i] > arr[i-1]) {
+                answer.add(arr[i]);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main01 T = new Main01();
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
+/**
+ * 1. 큰 수 출력하기
+ *
+ * 설명:
+ * N(1<=N<=100)개의 정수를 입력받아, 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램을 작성하세요.
+ * (첫 번째 수는 무조건 출력합니다.)
+ *
+ * 입력:
+ * - 첫 줄에 자연수 N(1 <= N <= 100)이 주어진다.
+ * - 그 다음 줄에 N개의 정수가 입력된다.
+ *
+ * 출력:
+ * - 자신의 바로 앞 수보다 큰 수만 한 줄로 출력한다.
+ *
+ * 예시 입력 1:
+ * 6
+ * 7 3 9 5 6 12
+ *
+ * 예시 출력 1:
+ * 7 9 6 12
+ */
